380f7fd7ae223094dd39718cd274b717b570827e
michalk94 <michal.kwit94@gmail.com>
2018-12-11 13:45:25 +0100

    Creating and defining UserDto class with fields. + using lombok

---
+++ src/main/java/congerence/room/manager/demoapp/user/UserDto.java
@@ -0,0 +1,15 @@
+package congerence.room.manager.demoapp.user;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private String name;
+
+    private String lastName;
+
+    private String login;
+
+    private String password;
+}
