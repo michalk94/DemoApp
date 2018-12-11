95ec6fb1404ae8cb0093267bb1daba5b49694892
michalk94 <michal.kwit94@gmail.com>
2018-12-11 15:01:41 +0100

    Adding sample data to database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
 insert into user(name, last_name, login, password) values
 ('John', 'Smith', 'jsmith', 'qwerty'),
 ('Jane', 'Doe', 'jdoe', 'mySecret');
+
+insert into room(room_name, location_description, number_of_seats, projector, phone_number) values
+('Large Room', '1st floor', 10, true, '22-22-22-22'),
+('Medium Room', '1st floor', 6, true, '22-22-22-23'),
+('Small Room', '2nd floor', 4, false, '22-11-22-11');
