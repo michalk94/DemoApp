da858d96a049f655bca5318b425ffd109fc9c99f
michalk94 <michal.kwit94@gmail.com>
2018-12-11 13:51:31 +0100

    Defining UserMapper, adding toDto method.

--- src/main/java/congerence/room/manager/demoapp/user/UserMapper.java
+++ src/main/java/congerence/room/manager/demoapp/user/UserMapper.java
@@ -1,4 +1,15 @@
 package congerence.room.manager.demoapp.user;
 
 public class UserMapper {
+
+    static UserDto toDto(User user){
+        UserDto dto = new UserDto();
+        dto.setName(user.getName());
+        dto.setLastName(user.getLastName());
+        dto.setLogin(user.getLogin());
+        dto.setPassword(user.getPassword());
+        return dto;
+    }
+
+    
 }
