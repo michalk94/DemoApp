063cd349168572dd417a1bd31e5efb84d5abc99b
michalk94 <michal.kwit94@gmail.com>
2018-12-11 14:50:06 +0100

    Creating room controller

---
+++ src/main/java/congerence/room/manager/demoapp/room/RoomResource.java
@@ -0,0 +1,10 @@
+package congerence.room.manager.demoapp.room;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rooms")
+public class RoomResource {
+}
