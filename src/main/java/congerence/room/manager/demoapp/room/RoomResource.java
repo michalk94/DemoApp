6402d414e66aa6e9c307f5d3fc456e4775fd5832
michalk94 <michal.kwit94@gmail.com>
2018-12-11 14:57:20 +0100

    Defining controller, simillar like with User, so without getting into details, Get,create,edit and delete room.

--- src/main/java/congerence/room/manager/demoapp/room/RoomResource.java
+++ src/main/java/congerence/room/manager/demoapp/room/RoomResource.java
@@ -1,10 +1,53 @@
 package congerence.room.manager.demoapp.room;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rooms")
 public class RoomResource {
+
+    @Autowired
+    private RoomService roomService;
+
+    public RoomResource(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping("")
+    List<RoomDto> findAll() {
+        return roomService.findAll();
+    }
+
+    @PostMapping("/create")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<RoomDto> createRoom(@RequestBody RoomDto roomDto) {
+        RoomDto savedRoom = roomService.createRoom(roomDto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{roomName}")
+                .buildAndExpand(savedRoom.getRoomName())
+                .toUri();
+        return ResponseEntity.created(location).body(savedRoom);
+    }
+
+    @PutMapping("/{roomName}")
+    public ResponseEntity<RoomDto> update(@PathVariable String roomName, @RequestBody RoomDto roomDto) {
+        RoomDto updatedRoom = roomService.editRoom(roomDto);
+        return ResponseEntity.ok(updatedRoom);
+    }
+
+    @DeleteMapping("/delete/{roomName}")
+    public ResponseEntity<RoomDto> deleteRoom(@PathVariable String roomName) {
+        roomService.deleteUser(roomName);
+        return ResponseEntity.ok().build();
+    }
+
 }
