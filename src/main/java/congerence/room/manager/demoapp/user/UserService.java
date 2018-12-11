e7a26611a70a1a4516d094ee5fea026e2992f970
michalk94 <michal.kwit94@gmail.com>
2018-12-11 13:59:10 +0100

    Defining service class, adding constructor and first funcionality to find all users.

--- src/main/java/congerence/room/manager/demoapp/user/UserService.java
+++ src/main/java/congerence/room/manager/demoapp/user/UserService.java
@@ -1,10 +1,28 @@
 package congerence.room.manager.demoapp.user;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    List<UserDto> findAll() {
+        return userRepository.findAll()
+                .stream()
+                .map(UserMapper::toDto)
+                .collect(Collectors.toList());
+    }
     
+
 }
