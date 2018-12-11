a49ca0342d13ec2376d60b28529d093c9b2ad117
michalk94 <michal.kwit94@gmail.com>
2018-12-11 16:01:18 +0100

    Defining RestController for booking

--- src/main/java/congerence/room/manager/demoapp/booking/BookingResource.java
+++ src/main/java/congerence/room/manager/demoapp/booking/BookingResource.java
@@ -1,12 +1,53 @@
 package congerence.room.manager.demoapp.booking;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/bookings")
 public class BookingResource {
 
-    
+    @Autowired
+    BookingService bookingService;
+
+    public BookingResource(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @GetMapping("")
+    List<BookingDto> findAll() {
+        return bookingService.findAll();
+    }
+
+    @GetMapping("/room/{roomName}")
+    List<BookingDto> findBookinForSingleRoom(@PathVariable String roomName) {
+        return bookingService.getBookinForSingleRoom(roomName);
+    }
+
+    @GetMapping("/user/{login}")
+    List<BookingDto> findBookinForSingleUser(@PathVariable String login) {
+        return bookingService.getBookinForSingleUser(login);
+    }
+
+    @PostMapping("/create")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<BookingDto> createRoom(@RequestBody BookingDto bookingDto) {
+        BookingDto savedBooking = bookingService.createBooking(bookingDto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedBooking.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(savedBooking);
+
+    }
 }
+
+
