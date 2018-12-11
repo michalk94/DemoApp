7edf055972496c064b9572046c1dcaf4c903df92
michalk94 <michal.kwit94@gmail.com>
2018-12-11 14:19:25 +0100

    Mapping funcionalities for getting, adding users.

--- src/main/java/congerence/room/manager/demoapp/user/UserResource.java
+++ src/main/java/congerence/room/manager/demoapp/user/UserResource.java
@@ -1,12 +1,41 @@
 package congerence.room.manager.demoapp.user;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
-    
+    @Autowired
+    private UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    List<UserDto> findAll() {
+        return userService.findAll();
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        UserDto savedUser = userService.createUser(userDto);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{login}")
+                .buildAndExpand(savedUser.getLogin())
+                .toUri();
+        return ResponseEntity.created(location).body(savedUser);
+
+    }
 }
