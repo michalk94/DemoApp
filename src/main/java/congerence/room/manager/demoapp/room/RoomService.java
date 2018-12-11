9b3cdceb529f5288a2775772726bb08d7d97ba85
michalk94 <michal.kwit94@gmail.com>
2018-12-11 14:49:14 +0100

    Delete rooms -> done

--- src/main/java/congerence/room/manager/demoapp/room/RoomService.java
+++ src/main/java/congerence/room/manager/demoapp/room/RoomService.java
@@ -18,14 +18,14 @@ public class RoomService {
         this.roomRepository = roomRepository;
     }
 
-    List<RoomDto> findAll(){
+    List<RoomDto> findAll() {
         return roomRepository.findAll()
                 .stream()
                 .map(RoomMapper::toDto)
                 .collect(Collectors.toList());
     }
 
-    RoomDto createRoom(RoomDto roomDto){
+    RoomDto createRoom(RoomDto roomDto) {
         Room roomEntity = RoomMapper.toEntity(roomDto);
         Room createRoom = roomRepository.save(roomEntity);
         return RoomMapper.toDto(createRoom);
@@ -38,5 +38,15 @@ public class RoomService {
         return mapAndSaveRoom(dbRoom, roomDto);
     }
 
-    
+    private RoomDto mapAndSaveRoom(Room dbRoom, RoomDto roomDto) {
+        dbRoom = RoomMapper.toEntity(dbRoom, roomDto);
+        Room savedRoom = roomRepository.save(dbRoom);
+        return RoomMapper.toDto(savedRoom);
+    }
+
+    void deleteUser(String roomId) {
+        roomRepository.deleteById(roomId);
+    }
+
+
 }
