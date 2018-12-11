0a892a1abc0a455744dc4a67b2058b026f7fb0b3
michalk94 <michal.kwit94@gmail.com>
2018-12-11 13:42:16 +0100

    Defining User class and adding sample data to data file

--- src/main/java/congerence/room/manager/demoapp/user/User.java
+++ src/main/java/congerence/room/manager/demoapp/user/User.java
@@ -1,4 +1,30 @@
 package congerence.room.manager.demoapp.user;
 
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class User {
+
+    private String name;
+
+    private String LastName;
+
+    @Id
+    private String login;
+
+    public User() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user(name, last_name, login, password) values
+('John', 'Smith', 'jsmith', 'qwerty'),
+('Jane', 'Doe', 'jdoe', 'mySecret');
