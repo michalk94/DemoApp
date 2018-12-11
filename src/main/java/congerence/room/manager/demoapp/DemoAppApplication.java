9a1a983415605f22c18218a5cee4d2dc9b5ea109
michalk94 <michal.kwit94@gmail.com>
2018-12-11 13:23:36 +0100

    Creating application using SpringBoot, adding Web, Lombok, Jpa, H2 dependencies

---
+++ src/main/java/congerence/room/manager/demoapp/DemoAppApplication.java
@@ -0,0 +1,12 @@
+package congerence.room.manager.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAppApplication.class, args);
+    }
+}
